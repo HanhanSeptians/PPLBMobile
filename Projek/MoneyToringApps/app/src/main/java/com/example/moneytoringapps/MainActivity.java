3e06367fe99794a99ca5042914c9a79ac3458f44
HanhanSeptians <hanhan.septian.tif20@polban.ac.id>
2022-03-08 19:38:37 +0700

    fragment dan recycleview

---
+++ Projek/MoneyToringApps/app/src/main/java/com/example/moneytoringapps/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.moneytoringapps;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigation;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigation = findViewById(R.id.bottomNavigation);
+        bottomNavigation.setOnItemReselectedListener(new NavigationBarView.OnItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+                switch (item.getItemId()){
+                    case R.id.pendapatan:
+                        selectedFragment = new pendapatanFragment();
+                        break;
+                    case R.id.home:
+                        selectedFragment = new homeFragment();
+                        break;
+                    case R.id.pengeluaran:
+                        selectedFragment = new pengeluaranFragment();
+                        break;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+            }
+        });
+    }
+}
